a6773906879174582290d0afe1c22d57610aadbe
yuya3145 <yuyayamada3145@gmail.com>
2025-02-08 19:38:46 +0900

    kadai_020

---
+++  kadai_020/event.js
@@ -0,0 +1,8 @@
+const text = document.getElementById('text'); 
+const btn = document.getElementById('btn');  
+
+btn.addEventListener('click', () => {
+  text.textContent = 'ボタンをクリックしました'; 
+});
+
+
